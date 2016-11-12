252ab6d283fd624f11b14bf4c4cf03ba8cdf8e13
devarchi33 <devarchi33@gmail.com>
2016-11-13 05:28:48 +0900

    Apply router 1st step.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-import React from 'react';
-const App = () => <h1>Hello React!</h1>
+import React, {Component} from 'react';
+import {Router, Route, hashHistory} from 'react-router';
+
+const Home = () => <div><h1>Home</h1></div>
+const About = () => <div><h1>About</h1></div>
+const Contact = () => <div><h1>Contact</h1></div>
+
+class App extends Component {
+    render() {
+        return (
+            <Router history = {hashHistory}>
+                <Route path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+            </Router>
+        );
+    }
+}
+
 export default App;
