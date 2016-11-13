f5b9203d10fb8d7122c2c78762f6e13637182886
devarchi33 <devarchi33@gmail.com>
2016-11-13 13:10:48 +0900

    Named Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
-import {Router, Route, Link, hashHistory} from 'react-router';
+import {Router, Route, IndexRoute, Link, hashHistory} from 'react-router';
 
-const Message = (props) => <div><h1>{props.params.message || 'Hello'}</h1><Links/></div>
+const Home = () => <h1>Home Page</h1>
+const HomeBody = () => <div><p>This is home page.</p></div>
+const Other = () => <h1>Other Page</h1>
+const OtherBody = () => <div><p>This is other page.</p></div>
+
+const Container = (props) => <div>{props.header}{props.body}<Links/></div>
 
 const Links = () => (
     <nav>
-        <Link activeStyle={{color: 'green'}} to="/">Hello</Link>
-        <Link activeStyle={{color: 'green'}} to="/Hi">Hi</Link>
-        <Link activeClassName="active" to="/Ni_Hao">Ni_Hao</Link>
+        <Link to="/">Home</Link>
+        <Link to="/other">Other</Link>
     </nav>
 )
 
@@ -15,7 +19,11 @@ class App extends Component {
     render() {
         return (
             <Router history = {hashHistory}>
-                <Route path="/(:message)" component={Message}>
+                <Route path="/" component={Container}>
+                    <IndexRoute components={ {header : Home, body : HomeBody} }>
+                    </IndexRoute>
+                    <Route path="/other" components={ {header : Other, body : OtherBody} }>
+                    </Route>
                 </Route>
             </Router>
         );
