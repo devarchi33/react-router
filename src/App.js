c106c51a5307946d152c78c41e5f56c5329d5a12
devarchi33 <devarchi33@gmail.com>
2016-11-13 11:33:53 +0900

    Apply params.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 import {Router, Route, Link, hashHistory} from 'react-router';
 
-const Home = (props) => <div><h1>Home</h1><Links/>{props.children}</div>
-const About = (props) => <div><h1>About</h1>{props.children}</div>
-const Contact = () => <div><h1>Contact</h1></div>
+const Message = (props) => <div><h1>{props.params.message || 'Hello'}</h1><Links/></div>
 
 const Links = () => (
     <nav>
-        <Link activeStyle={{color: 'green'}} to="/">Home</Link>
-        <Link activeStyle={{color: 'green'}} to="/about">About</Link>
-        <Link activeClassName="active" to="/about/contact">Contact</Link>
+        <Link activeStyle={{color: 'green'}} to="/">Hello</Link>
+        <Link activeStyle={{color: 'green'}} to="/Hi">Hi</Link>
+        <Link activeClassName="active" to="/Ni_Hao">Ni_Hao</Link>
     </nav>
 )
 
@@ -17,10 +15,7 @@ class App extends Component {
     render() {
         return (
             <Router history = {hashHistory}>
-                <Route path="/" component={Home}>
-                    <Route path="about" component={About}>
-                        <Route path="contact" component={Contact}/>
-                    </Route>
+                <Route path="/(:message)" component={Message}>
                 </Route>
             </Router>
         );
