d6acfecaf357eb7407696b8755c10c59ea5dbc44
devarchi33 <devarchi33@gmail.com>
2016-11-13 13:43:43 +0900

    Querystring Prams

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, {Component} from 'react';
 import {Router, Route, IndexRoute, Link, hashHistory} from 'react-router';
 
-const Home = () => <h1>Home Page</h1>
-const HomeBody = () => <div><p>This is home page.</p></div>
-const Other = () => <h1>Other Page</h1>
-const OtherBody = () => <div><p>This is other page.</p></div>
-
-const Container = (props) => <div>{props.header}{props.body}<Links/></div>
+const Page = (props) => <div><h1>{props.location.query.message || 'Hello'}</h1><Links/></div>
 
 const Links = () => (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/other">Other</Link>
+        <Link to={ { pathname : "/", query : {message : ''} } }>Hello</Link>
+        <Link to={ { pathname : "/", query : {message : 'Yop'} } }>Yop</Link>
     </nav>
 )
 
 class App extends Component {
-    render() {
+    render () {
         return (
-            <Router history = {hashHistory}>
-                <Route path="/" component={Container}>
-                    <IndexRoute components={ {header : Home, body : HomeBody} }>
-                    </IndexRoute>
-                    <Route path="/other" components={ {header : Other, body : OtherBody} }>
-                    </Route>
+            <Router history={hashHistory}>
+                <Route path="/" component={Page}>
                 </Route>
-            </Router>
+            </Router>        
         );
     }
 }
